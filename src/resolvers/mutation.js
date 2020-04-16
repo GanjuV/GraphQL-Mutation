451b5e222b578d8fb87c4ad7d263aceeccd9b573
GanjuV <vaibhav34@gmail.com>
2020-04-16 23:53:41 +0530

    Added update methods for post and comment

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -71,6 +71,22 @@ const mutation = {
     return deletePost[0]
 
   },
+  updatePost (parent, { id, data}, { db }, info) {
+    const post = db.posts.find(post => post.id === id)
+    if (!post) {
+      throw new Error("Post not found")
+    }
+    if (typeof data.title === 'string') {
+      post.title = data.title
+    }
+    if (typeof data.body === 'string') {
+      post.body = data.body
+    }
+    if (typeof data.published === 'boolean') {
+      post.published = data.published
+    }
+    return post
+  },  
   createComment(parent, args, { db }, info) {
     const userExist = db.users.some(user => user.id === args.comment.author)
     const postExist = db.posts.some(post => post.id === args.comment.post && post.published)
@@ -91,6 +107,16 @@ const mutation = {
     }
     const deleteComment = db.comments.splice(commentIndex, 1)
     return deleteComment[0]
+  },
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find(comment => comment.id === id)
+    if (!comment) {
+      throw new Error("Comment not found")
+    }
+    if (typeof data.text === 'string') {
+      comment.text = data.text
+    }
+    return comment
   }
 }
 
